9a31f7e4323c1dcc07dcf47544863110529191d3
RENZO SARMIENTO <renzo.sarmiento@tecsup.edu.pe>
2024-05-19 10:49:12 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const http = requi-re('http');
+
+const port = 3000; 
+
+const server = http.createServer((req, res) => {
+
+res.statusCode = 200;
+
+res.setHeader('Content-Type','text/plain');
+
+res.end('Hola a todos les dice su docen-te');
+
+});
+
+server.listen(port,() => {
+
+console.log('Server corriendo en el puerto 3000');
+
+});
